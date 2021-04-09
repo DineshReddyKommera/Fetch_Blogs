ebeccef53de9bc8bb4dc4f230626687031d08036
DineshReddyKommera <dineshreddykommera@gmail.com>
2021-04-09 10:34:03 -0700

    Added new route for fetching all posts

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
 const app = express();
+//Parsing incoming requests bodies
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+// Better than fetch module for testing and making requests
+const axios = require('axios');
+
+app.use(bodyParser.json());
+
+//Middleware used for logging requests to the application.
+app.use(morgan('dev'));
 
 //Registering the default port if available
 const PORT = process.env.PORT || 2222;
@@ -8,8 +18,22 @@ const PORT = process.env.PORT || 2222;
 
 //Basic API testing
 app.get('/api/ping', (req, res) => {
-  res.status(200).send("Success");
+    res.status(200).send({
+        success: 'true',
+    })
 })
+
+//Fetching all posts
+app.get('/api/posts', (req, res) => {
+    //Replace with the custom url
+    axios.get('http://localhost/blog/posts')
+    .then(response => {
+      res.send(response.data)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  })
 
 
 app.listen(PORT, () => {
