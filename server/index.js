9e4947248e8d78a22c94ca7f4bbc32d364f559f1
DineshReddyKommera <dineshreddykommera@gmail.com>
2021-04-09 11:31:34 -0700

    Started initial coding for sortBy Functionality

--- server/index.js
+++ server/index.js
@@ -26,18 +26,122 @@ app.get('/api/ping', (req, res) => {
 //Fetching all posts
 app.get('/api/posts', (req, res) => {
     //Replace with the custom url
+     // Gets all posts with at least one tag from the API using axios
+  axios.all([
+    axios.get('http://localhost:2222/blog/posts?tag=tech'),
+    axios.get('http://localhost:2222/blog/posts?tag=history'),
+    axios.get('http://localhost:2222/blog/posts?tag=health'),
+    axios.get('http://localhost:2222/blog/posts?tag=startups'),
+    axios.get('http://localhost:2222/blog/posts?tag=science'),
+    axios.get('http://localhost:2222/blog/posts?tag=design'),
+    axios.get('http://localhost:2222/blog/posts?tag=culture'),
+    axios.get('http://localhost:2222/blog/posts?tag=politics'),
+    axios.get('http://localhost:2222/blog/posts?tag=science')
+  ])
+  .then(axios.spread((response1, response2, response3, response4, response5, response6, response7, response8, response9) => {
+    // Organizes data into an array
+    let data = [
+      response1.data.posts,
+      response2.data.posts,
+      response3.data.posts,
+      response4.data.posts,
+      response5.data.posts,
+      response6.data.posts,
+      response7.data.posts,
+      response8.data.posts,
+      response9.data.posts
+    ];
+    // Object so that a hash can be made on the id of the post and remove duplicates
+    let post = {};
+    let posts = [];
+    for (let i = 0; i < data.length; i++) {
+      let blog = data[i];
+      for (let i = 0; i < blog.length; i++) {
+        post[blog[i].id] = blog[i];
+      }
+    }
+    // Create response object so that the result of the request is in the correct format
+    for (let key in post) {
+      posts.push(post[key]);
+    }
+    let response = {
+      posts,
+    }
+    res.status(200).send(response);
+  }))
+  .catch(error => {
+    res.status(400).send({
+      error: 'Tags parameter is required'
+    })
+    console.log(error)
+  });
+})
+
+// Initial code for sortBy functionality
+app.get('/api/posts/sortBy', (req, res) => {
     axios.all([
-        axios.get('http://localhost:2222/api/blog/posts?tag=item1'),
-        axios.get('http://localhost:2222/api/blog/posts?tag=item2')
+        axios.get('http://localhost:2222/blog/posts?tag=tech'),
+        axios.get('http://localhost:2222/blog/posts?tag=history')
       ])
       .then(axios.spread((response1, response2) => {
-        const data1 = response1.data.posts;
-        const data2 = response2.data.posts;
-        const posts = data1.concat(data2)
-        const post = {
-          posts
+        let data = [
+            response1.data.posts,
+            response2.data.posts
+          ];
+          // Object so that a hash can be made on the id of the post and remove duplicates
+          let post = {};
+          let posts = [];
+          for (let i = 0; i < data.length; i++) {
+            let blog = data[i];
+            for (let i = 0; i < blog.length; i++) {
+              post[blog[i].id] = blog[i];
+            }
+          }
+          // Create response object so that the result of the request is in the correct format
+          for (let key in post) {
+            posts.push(post[key]);
+          }
+      
+          // Merge Sort function so that the data is sorted quickly
+          // Sorting is notoriously slow so this is a great place to optimize code
+          const merge = (arr1, arr2) => {
+            let result = [];
+            var i = 0;
+            var j = 0;
+            while(i < arr1.length && j < arr2.length) {
+              if (arr2[j].likes < arr1[i].likes) {
+                result.push(arr1[i]);
+                i++;
+              } else {
+                result.push(arr2[j]);
+                j++;
+              }
+            }
+            while(i < arr1.length) {
+              result.push(arr1[i]);
+              i++
+            }
+            while(j < arr2.length) {
+              result.push(arr2[j]);
+              j++
+            }
+            return result;
         }
-        res.status(200).send(post);
+        const mergeSort = (arr) => {
+            if (arr.length <= 1) {
+              return arr;
+            }
+            let mid = Math.floor(arr.length / 2);
+            let left = mergeSort(arr.slice(0, mid));
+            let right = mergeSort(arr.slice(mid));
+            return merge(left, right);
+          }
+          // Calling Merge sort on the retrieved data
+          let sortedPosts = mergeSort(posts);
+          let response = {
+            posts: sortedPosts
+          }
+          res.status(200).send(response);
       }))
       .catch(error => {
         res.status(400).send({
